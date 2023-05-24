9f77c203e9231da70e46ad5b51ec96034bfb30d4
mjfeeney <mjfeeney3@gmail.com>
2023-05-24 12:59:51 -0400

    Updated js files
    Finished part 1 of project.  Created Game and Phrase classes in their own js files and updated app.js file.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+    }
+
+    missed = 0;
+    phrases = [
+        new Phrase("Phrase number one"), 
+        new Phrase("This is a second phrase"), 
+        new Phrase("New phrase NUMBER 3")
+    ];
+    activePhrase = null;
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+
+    getRandomPhrase() {
+        let randomNumber = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomNumber];
+    };
+
+    startGame() {
+        const overlay = document.getElementById("overlay");
+        overlay.style.display = "none";
+        
+        this.activePhrase = this.getRandomPhrase();
+        console.log(this.activePhrase);
+        this.activePhrase.addPhraseToDisplay();
+
+    };
+}
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+// create a new instance of the `Game` class and add event listeners for the start button and onscreen keyboard buttons
+
+let game;
+const btn_reset = document.getElementById("btn__reset");
+
+btn_reset.addEventListener("click", function() {
+    game = new Game();
+    game.startGame();
+});
+
+
+
+
+
+
